a4f75a5f5f3d30f74896d9568b61384f811c6368
Jay Calderon <cald0145@algonquinlive.com>
2024-07-26 23:40:07 -0400

    Add SafeAreaView and LinearGradient components for improved UI styling

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,8 @@
-import { View, Text, ImageBackground } from "react-native";
+import { View, Text, ImageBackground, SafeAreaView } from "react-native";
 import React from "react";
+import { LinearGradient } from "expo-linear-gradient";
 
-import beachImage from "@/assets/meditation-images/beach.webp";
+import beachImage from "../assets/meditation-images/beach.webp";
 
 const App = () => {
   return (
@@ -11,7 +12,21 @@ const App = () => {
         resizeMode="cover"
         className="flex-1"
       >
-        <Text>App</Text>
+        <LinearGradient
+          className="flex-1"
+          colors={["rgba(0, 0, 0, 0.4)", "rgba(0, 0, 0, 0.8)"]}
+        >
+          <SafeAreaView className="flex-1 px-1 justify-between">
+            <View>
+              <Text className="text-center text-white font-bold text-4xl">
+                Simple Meditation
+              </Text>
+              <Text className="text-center text-white text-regular text-2xl mt-3">
+                Simplifying Meditation for Everyone!
+              </Text>
+            </View>
+          </SafeAreaView>
+        </LinearGradient>
       </ImageBackground>
     </View>
   );
