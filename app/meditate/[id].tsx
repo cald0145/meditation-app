0f65f32872bcbf53db6a0a07c28ce21535e4bb4d
Jay Calderon <cald0145@algonquinlive.com>
2024-08-16 19:28:52 -0400

    Add countdown timer and start button to meditation screen

--- app/meditate/[id].tsx
+++ app/meditate/[id].tsx
@@ -1,28 +1,64 @@
 import { View, Text, ImageBackground, Pressable } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import MEDITATION_IMAGES from "@/constants/meditation-images";
 import AppGradient from "@/components/AppGradient";
-import { router } from "expo-router";
+import { router, useLocalSearchParams } from "expo-router";
 import AntDesign from "@expo/vector-icons/AntDesign";
+import CustomButton from "@/components/CustomButton";
 
 const Meditate = () => {
+  const { id } = useLocalSearchParams();
+  const [secondsRemaining, setSecondsRemaining] = useState(10);
+  const [isMeditating, setMeditating] = useState(false);
+
+  useEffect(() => {
+    let timerId: NodeJS.Timeout;
+
+    if (secondsRemaining === 0) {
+      setMeditating(false);
+      return;
+    }
+
+    if (isMeditating) {
+      timerId = setTimeout(() => {
+        setSecondsRemaining(secondsRemaining - 1);
+      }, 1000);
+    }
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [secondsRemaining, isMeditating]);
+
   return (
     <View className="flex-1">
       <ImageBackground
-        source={MEDITATION_IMAGES[0]}
+        source={MEDITATION_IMAGES[Number(id) - 1]}
         resizeMode="cover"
         className="flex-1"
       >
-        <AppGradient
-          colors={["transparent", "rgba(0, 0, 0, 0.8)"]}
-        ></AppGradient>
-        <Pressable
-          onPress={() => router.back()}
-          className="absolute top-16 left-6 z-10"
-        >
-          <AntDesign name="leftcircleo" size={40} color="white" />
-        </Pressable>
+        <AppGradient colors={["transparent", "rgba(0, 0, 0, 0.8)"]}>
+          <Pressable
+            onPress={() => router.back()}
+            className="absolute top-16 left-6 z-10"
+          >
+            <AntDesign name="leftcircleo" size={40} color="white" />
+          </Pressable>
+          <View className="flex-1 justify-center">
+            <View className="mx-auto bg-neutral-200 rounded-full w-44 h-44 justify-center items-center">
+              <Text className="text-4xl text-blue-800">
+                00:{secondsRemaining}
+              </Text>
+            </View>
+          </View>
+          <View className="mb-5">
+            <CustomButton
+              title="Start Meditation"
+              onPress={() => setMeditating(true)}
+            />
+          </View>
+        </AppGradient>
       </ImageBackground>
     </View>
   );
